d56497c7879fc5eddd3084a2acf563998323a9d4
Daksh <goyaldaksh5522@gmail.com>
2024-05-28 21:48:03 +0530

    update strict mode

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -7,7 +7,6 @@ import store from './store/index.js'
 import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
 
     <Provider store={store}>
       <BrowserRouter >
@@ -15,5 +14,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </BrowserRouter>
     </Provider>
   
-  </React.StrictMode>,
 )
